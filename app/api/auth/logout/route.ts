9ba31cf433a08724ca7f4df6ddd4ef701c74fa97
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-02-22 22:03:48 -0500

    Refactor logout route to use clearSession

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,10 +1,11 @@
 // app/api/auth/logout/route.ts
+
 import { NextResponse } from "next/server";
-import { clearSessionCookie } from "@/lib/session";
+import { clearSession } from "@/lib/session";
 
 export async function GET(req: Request) {
+  clearSession();
+
   const url = new URL(req.url);
-  const res = NextResponse.redirect(new URL("/", url.origin));
-  clearSessionCookie(res);
-  return res;
+  return NextResponse.redirect(new URL("/", url.origin));
 }
