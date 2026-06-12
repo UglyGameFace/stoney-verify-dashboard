e4a0fe8416dca875cfb02a6e6755441f2abf2d4b
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-06-12 15:33:20 -0400

    Add Next TypeScript global env file

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference types="styled-jsx" />
+
+// This file is automatically consumed by Next.js and TypeScript.
+// Do not edit manually unless Next/TypeScript global types change.
